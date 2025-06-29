229988dc918f3770fbac3abb99f30cdb92724b13
Aditya A <adityaarun2006@gmail.com>
2025-06-29 14:00:04 +0530

    Added Loader Element

--- src/components/Loading.jsx
+++ src/components/Loading.jsx
@@ -10,7 +10,9 @@ export default function Loader() {
           <circle className="opacity-25" cx="12" cy="12" r="10" stroke="currentColor" strokeWidth="4" fill="none" />
           <path className="opacity-75" fill="currentColor" d="M4 12a8 8 0 018-8v8z" />
         </svg>
-        {Math.floor(progress)}% Loading...
+        <div className="w-full">
+          {Math.floor(progress)}% Loading...
+        </div>
       </div>
     </Html>
   );
